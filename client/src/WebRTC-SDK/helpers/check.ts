56fdc2f76b49eef9f840e34d656cdfc9f379e500
anyuxuan <anyuxuan0701@163.com>
2019-03-14 10:07:04 +0800

    feat: 💄detectRTC function

---
+++ client/src/WebRTC-SDK/helpers/check.ts
@@ -0,0 +1,24 @@
+import { isChrome, getBrowserVersion, isFirefox, isOpera, isSafari } from '../utils';
+
+export function detectRTC(): boolean {
+  let isSupport = false;
+  // @ts-ignore
+  const pc = !!(window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection);
+  // @ts-ignore
+  const hasRTCApi = !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia || navigator.mozGetUserMedia || (navigator.mediaDevices && navigator.mediaDevices.getUserMedia));
+  // @ts-ignore
+  const hasWebSocket = !!window.WebSocket;
+  if (isChrome() && getBrowserVersion() >= 56) {
+    isSupport = true;
+  }
+  if (isFirefox() && getBrowserVersion() >= 22) {
+    isSupport = true;
+  }
+  if (isOpera() && getBrowserVersion() >= 43) {
+    isSupport = true;
+  }
+  if (isSafari() && getBrowserVersion() >= 11) {
+    isSupport = true;
+  }
+  return isSupport && pc && hasRTCApi && hasWebSocket;
+}
--- client/src/WebRTC-SDK/utils/browser.ts
+++ client/src/WebRTC-SDK/utils/browser.ts
@@ -8,17 +8,6 @@ interface OSInfo {
 const uaParser = new UAParser();
 const uaResult = uaParser.getResult();
 
-export function detectRTC(): boolean {
-  let isSupport = false;
-  // @ts-ignore
-  const pc = !!(window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection);
-  // @ts-ignore
-  const hasRTCApi = !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia || navigator.mozGetUserMedia || (navigator.mediaDevices && navigator.mediaDevices.getUserMedia));
-  // @ts-ignore
-  const hasWebSocket = !!window.WebSocket;
-  return isSupport && pc && hasRTCApi && hasWebSocket;
-}
-
 export function isChrome(): boolean {
   return uaResult.browser.name === 'Chrome';
 }
