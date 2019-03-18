c56c638cc62a1576c565ea1dcce3e055cf3e4227
anyuxuan <anyuxuan0701@163.com>
2019-03-18 10:22:56 +0800

    feat: add initCore function

--- client/src/WebRTC-SDK/Client.ts
+++ client/src/WebRTC-SDK/Client.ts
@@ -1,13 +1,31 @@
-import { SDKConfig } from './WebRTC-SDK';
+import { SDKConfig } from '@/WebRTC-SDK/WebRTC-SDK';
+import { isString, isFunction } from '@/WebRTC-SDK/utils/value-check';
 
 export interface ClientProps {
 }
 
 class Client implements ClientProps {
   private config: SDKConfig;
+  private appId: string;
 
   constructor(config: SDKConfig) {
     this.config = config;
+  }
+
+  initCore(appId: string, success: () => void, fail: (err: Error) => void): void {
+    if (!appId || !isString(appId)) {
+      if (isFunction(fail)) {
+        fail(new Error());
+      }
+    }
+    this.appId = appId;
+    if (isFunction(success)) {
+      success();
+    }
+  }
+
+  enterRoom() {
+
   }
 }
 
---
+++ client/src/WebRTC-SDK/utils/value-check.ts
@@ -0,0 +1,15 @@
+const toString = Object.prototype.toString;
+
+const isType = (type: string) => (val: any): boolean => toString.call(val) === `[object ${type}]`;
+
+export const isEmpty = (val: any): boolean => {
+  return val == null || !(Object.keys(val) || val).length;
+};
+
+export const isPlainObject = isType('Object');
+
+export const isString = isType('String');
+
+export const isFunction = isType('Function');
+
+
